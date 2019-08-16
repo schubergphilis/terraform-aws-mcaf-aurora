10f32711c012b4f0d0655ee018cb87c192b9b2b2
Tommy Maintz <tommy.maintz@gmail.com>
2019-08-16 11:13:53 +0200

    Also add database and username

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,16 @@ output "id" {
   description = "ID of the Aurora cluster"
 }
 
+output "database" {
+  value       = var.database
+  description = "The name of the first database to be created when the cluster is created"
+}
+
+output "username" {
+  value       = var.username
+  description = "Username for the master DB user"
+}
+
 output "endpoint" {
   value       = aws_rds_cluster.default.endpoint
   description = "The DNS address of the RDS instance"
