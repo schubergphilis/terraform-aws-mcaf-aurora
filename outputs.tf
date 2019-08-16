bd8c4c2c39203a3c3356666eb9c5c1588d216913
Tommy Maintz <tommy.maintz@gmail.com>
2019-08-16 11:10:29 +0200

    Add connection details of the rds cluster to the outputs of the module

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "id" {
   value       = aws_rds_cluster.default.id
   description = "ID of the Aurora cluster"
 }
+
+output "endpoint" {
+  value       = aws_rds_cluster.default.endpoint
+  description = "The DNS address of the RDS instance"
+}
+
+output "reader_endpoint" {
+  value       = aws_rds_cluster.default.reader_endpoint
+  description = "A read-only endpoint for the Aurora cluster, automatically load-balanced across replicas"
+}
+
+output "port" {
+  value       = aws_rds_cluster.default.port
+  description = "The database port"
+}
