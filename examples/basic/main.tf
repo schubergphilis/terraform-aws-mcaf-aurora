058836d59cfcfdf2ed5e5b6232d0a486db68cec9
Marwin Baumann <mbaumann@schubergphilis.com>
2023-03-21 10:01:27 +0100

    simplify basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,16 +1,9 @@
-locals {
-  create_db = true
-}
-
 resource "random_password" "master_password" {
-  count = local.create_db ? 1 : 0
-
   length  = "20"
   special = false
 }
 
 module "aurora" {
-  count  = local.create_db ? 1 : 0
   source = "../../"
 
   cidr_blocks                         = ["1.1.1.1/24"]
@@ -22,7 +15,7 @@ module "aurora" {
   iam_database_authentication_enabled = true
   instance_class                      = "db.t3.medium"
   instance_count                      = 1
-  password                            = random_password.master_password[0].result
+  password                            = random_password.master_password.result
   stack                               = "example"
   subnet_ids                          = ["subnet-00000000000000000", "subnet-00000000000000001"]
   username                            = "admin_user"
