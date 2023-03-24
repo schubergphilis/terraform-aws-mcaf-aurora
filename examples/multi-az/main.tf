fce5ec937963a3a8f4d4750536e0ec29cf7662f3
Stephen Hoekstra <shoekstra@schubergphilis.com>
2023-03-24 22:02:42 +0100

    Make examples more minimal
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- examples/multi-az/main.tf
+++ examples/multi-az/main.tf
@@ -1,6 +1,6 @@
 locals {
-  vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.0.0.0/16"
 }
 
 provider "aws" {
@@ -9,26 +9,26 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-resource "random_password" "master_password" {
-  length  = "20"
-  special = false
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name             = "example"
+  azs              = local.azs
+  cidr             = local.vpc_cidr
+  database_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
+  public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
 }
 
 module "aurora" {
-  source = "../../"
+  source = "../.."
 
-  cluster_family                      = "aurora-mysql8.0"
-  enabled_cloudwatch_logs_exports     = ["audit", "error", "general", "slowquery"]
-  engine                              = "aurora-mysql"
-  engine_mode                         = "provisioned"
-  engine_version                      = "8.0.mysql_aurora.3.02.2"
-  iam_database_authentication_enabled = true
-  instance_class                      = "db.t3.medium"
-  password                            = random_password.master_password.result
-  security_group_rules                = { ingress_allowed_cidr_blocks = [local.vpc_cidr] }
-  stack                               = "example"
-  subnet_ids                          = module.vpc.private_subnets
-  username                            = "admin_user"
+  stack       = "example"
+  engine_mode = "provisioned"
+  password    = "password"
+  subnet_ids  = module.vpc.private_subnets
+  username    = "admin_user"
 
   cluster_endpoints = {
     reader = {
@@ -46,40 +46,5 @@ module "aurora" {
       promotion_tier = 15,
       instance_class = "db.t3.medium"
     }
-  }
-
-  tags = {
-    env = "production"
-  }
-}
-
-// Supporting Resources
-module "vpc" {
-  #checkov:skip=CKV_AWS_111 False positive
-  #checkov:skip=CKV2_AWS_12: False positive
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name                                 = "example"
-  azs                                  = local.azs
-  cidr                                 = "10.0.0.0/16"
-  create_flow_log_cloudwatch_iam_role  = true
-  create_flow_log_cloudwatch_log_group = true
-  database_subnets                     = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 6)]
-  default_network_acl_name             = "example-default-nacl"
-  default_security_group_egress        = []
-  default_security_group_ingress       = []
-  default_security_group_name          = "example-default-sg"
-  enable_dns_hostnames                 = true
-  enable_dns_support                   = true
-  enable_flow_log                      = true
-  manage_default_network_acl           = true
-  manage_default_security_group        = true
-  map_public_ip_on_launch              = false
-  private_subnets                      = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 3)]
-  public_subnets                       = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
-
-  tags = {
-    env = "production"
   }
 }
