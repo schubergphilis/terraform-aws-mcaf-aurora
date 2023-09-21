51e5fdec07ecec202d187bf9aea4641e78495ef7
Marwin Baumann <mbaumann@schubergphilis.com>
2023-09-21 11:28:47 +0200

    security: solve latest checkov findings

--- examples/multi-az/main.tf
+++ examples/multi-az/main.tf
@@ -21,17 +21,24 @@ module "vpc" {
   public_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
 }
 
+module "kms" {
+  source = "github.com/schubergphilis/terraform-aws-mcaf-kms?ref=v0.3.0"
+  name   = "example"
+}
+
 // Multi-AZ DB clusters are not the same as Aurora DB clusters. For information about Aurora DB clusters, see the Amazon Aurora User Guide.
 // https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/CHAP_AuroraOverview.html
 module "aurora" {
   source = "../.."
 
-  name                      = "example"
-  allocated_storage         = 100
-  allowed_cidr_blocks       = [local.vpc_cidr]
-  db_cluster_instance_class = "db.r6gd.xlarge"
-  instance_count            = 3
-  iops                      = 1000
-  storage_type              = "io1"
-  subnet_ids                = module.vpc.private_subnets
+  name                          = "example"
+  allocated_storage             = 100
+  allowed_cidr_blocks           = [local.vpc_cidr]
+  db_cluster_instance_class     = "db.r6gd.xlarge"
+  instance_count                = 3
+  iops                          = 1000
+  kms_key_id                    = module.kms.arn
+  master_user_secret_kms_key_id = module.kms.arn
+  storage_type                  = "io1"
+  subnet_ids                    = module.vpc.private_subnets
 }
