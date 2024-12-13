c64417a6075efaed278ee555aa17def53624b322
Frank van Boven <frank@cenotaph.nl>
2024-12-13 11:51:52 +0100

    Remove Assert, not supported by tfsec

--- variables.tf
+++ variables.tf
@@ -336,7 +336,7 @@ variable "seconds_until_auto_pause" {
   description = "The time, in seconds, before an Aurora Serverless DB cluster is paused"
 
   validation {
-    condition     = provider::assert::between(300, 86400, var.seconds_until_auto_pause)
+    condition     = var.seconds_until_auto_pause >= 300 && var.seconds_until_auto_pause <= 86400
     error_message = "seconds_until_auto_pause must be between 300 (5 minutes) and 86400 (1 day)"
   }
 }
--- versions.tf
+++ versions.tf
@@ -1,13 +1,9 @@
 terraform {
   required_providers {
-    assert = {
-      source  = "hashicorp/assert"
-      version = ">= 0.14.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.81.0"
     }
   }
-  required_version = ">= 1.8"
+  required_version = ">= 1.3"
 }
